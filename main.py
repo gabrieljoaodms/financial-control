b21672eabac448bb7fb6c86e184b9b9d6067c4db
João Gabriel da Motta Silva <85176262+gabrieljoaodms@users.noreply.github.com>
2022-12-17 16:06:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import emoji
-from dotenv import load_dotenv
-import os
-from sqlalchemy import create_engine
-from obter_categoria import obter_categoria
 
-
-st.sidebar.title('Menu')
-paginaSelecionada = st.sidebar.selectbox('Selecione a página', ['Adicionar Receita', 'Adicionar Saída','Acrescentar Categoria', 'Aportes',
-                                                                'Saldo Investimentos', 'Ajustar Orçamento'])
+st.write('Hello')
